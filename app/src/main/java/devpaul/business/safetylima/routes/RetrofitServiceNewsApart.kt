4f700b3ba0ef37c781df270d46c5ee81caab1d09
paulguillen190 <paulguillen190@gmail.com>
2022-05-09 17:29:02 -0500

    AdaptingData

--- app/src/main/java/devpaul/business/safetylima/routes/RetrofitServiceNewsApart.kt
+++ app/src/main/java/devpaul/business/safetylima/routes/RetrofitServiceNewsApart.kt
@@ -7,19 +7,20 @@ import retrofit2.http.GET
 
 interface RetrofitServiceNewsApart {
 
-    @GET("v2/top-headlines?country=ar&apiKey=f206dd4aec1b46f38defa2ae5b0740e1")
+
+    @GET("v2/top-headlines?country=ar&apiKey=Your_Key_From_NewsAPI")
     fun  getDataArgentina (): Call<Data>
 
-    @GET("v2/top-headlines?country=co&apiKey=f206dd4aec1b46f38defa2ae5b0740e1")
+    @GET("v2/top-headlines?country=co&apiKey=Your_Key_From_NewsAPI")
     fun  getDataColombia (): Call<Data>
 
-    @GET("v2/top-headlines?country=cu&apiKey=f206dd4aec1b46f38defa2ae5b0740e1")
+    @GET("v2/top-headlines?country=cu&apiKey=Your_Key_From_NewsAPI")
     fun  getDataCuba (): Call<Data>
 
-    @GET("v2/top-headlines?country=mx&apiKey=f206dd4aec1b46f38defa2ae5b0740e1")
+    @GET("v2/top-headlines?country=mx&apiKey=Your_Key_From_NewsAPI")
     fun  getDataMexico (): Call<Data>
 
-    @GET("v2/top-headlines?country=ve&apiKey=f206dd4aec1b46f38defa2ae5b0740e1")
+    @GET("v2/top-headlines?country=ve&apiKey=Your_Key_From_NewsAPI")
     fun  getDataVenezuela (): Call<Data>
 
 }
